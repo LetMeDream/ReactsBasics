bbc95f4083d9e42c6a3f0bad1df498a51fe0702f
LetMeDream <raulalfonzo66@gmail.com>
2021-05-09 09:03:31 -0400

    Using some 'tricks' such as passing props through an object using spread operator, interpolating variables and using react.Fragment

--- react.js
+++ react.js
@@ -1,14 +1,24 @@
 let rootElement = document.getElementById('root');
 
-//React api in order to create the element
-/* let element = React.createElement('div', {
-    children: ['Hello world but from ReactJs'],
-    className: 'container'
-    }
-); */
-
 //Now we will recreate the exact same example from above but using JSX
 //In order fro the browser to be able to understand JSX, 'babel' must be used
-let element = <div className='container'>Hello world from JSX</div>;
+
+/* If we wanted to use variables as interpolated values */
+let greet = 'Hello from an interpolated JSX variable';
+let divClassName = 'container mt-3';
+
+let className = 'button is-small';
+let href = 'https://google.co.ve';
+let props = { className, href }
+
+let element =
+    <>
+        <div className={divClassName}>
+            {greet}<br/>
+            <a id='extended' { ...props } target='_blank'>New button</a>
+        </div>
+    </>;
+
+console.log(element);
 
 ReactDOM.render(element, rootElement);
