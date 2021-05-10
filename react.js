1d1f051a9fdaed8a883b018d8e2dbb23fb2b99cc
LetMeDream <raulalfonzo66@gmail.com>
2021-05-10 12:31:47 -0400

    Here we are just validating out component types using PropTypes on our development code

--- react.js
+++ react.js
@@ -1,16 +1,29 @@
+
 let rootElement = document.getElementById('root');
 
 let btnClasses = 'button is-danger is-outlined mx-1';
 
-/* Creating a component on React */
-let Btn = (props) => <a class={btnClasses} href={ props.url }>Going to { props.destiny }</a>;
+/* Creating a function component on React */
+const Btn = (props) => <a className={btnClasses} href={ props.url }>Going to { props.destiny }</a>;
+
+/* function Btn({url, destiny}){
+    return( <a className={btnClasses} href={ url }>Going to { destiny }</a> );
+} */
+
+/* Let's create custom validation to our custom function component */
+Btn.propTypes = {
+    destiny: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+}
+
 
 let element =
     <>
         <Btn destiny='4chan' url='https://4chan.org'></Btn>
         <Btn destiny='Youtube' url='https://www.youtube.com'></Btn>
+        <Btn destiny='Instagram' url='https://www.instagram.com'></Btn>
     </>;
 
-console.log(Btn);
+
 
 ReactDOM.render(element, rootElement);
