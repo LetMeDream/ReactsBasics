63778b771777676b6b82cad5d9432800964e0837
LetMeDream <raulalfonzo66@gmail.com>
2021-05-11 18:38:14 -0400

    Using localStorage to save the 'name' variable into our browsers memory

--- react.js
+++ react.js
@@ -1,36 +1,24 @@
 let rootElement = document.getElementById('root');
 
-let state = { count: 0 , msg: '' }
+function Greetings(){
 
-function App(){
+    const [name, setName] = React.useState(window.localStorage.getItem('name'));
 
-    function handleButton(){
-        setState( { count: state.count + 1 } );
-    }
-
-    function handleChange(event){
-        setState( { msg: event.target.value } );
-    }
+    React.useEffect( () => {
+        window.localStorage.setItem('name', name);
+        name ? document.title = `Welcome, ${name.charAt(0).toUpperCase() + name.slice(1) }` : document.title = "REACT'S BASICS" ;
+    });
 
     return(
         <div className='container mt-2'>
-            <button onClick={ handleButton } >Event me</button>
-            <p>The event has fired {state.count} times </p>
-            <p>The message written in the input below is: <strong>{ state.msg }</strong></p>
-            <input onChange={ handleChange } />
+            <label htmlFor="name">Name</label>
+            <br/>
+            <input value={name} onChange={ (event) => setName(event.target.value) } type="text"/>
+            <br/>
+            { name ?  <strong>Hello {name}</strong> : 'Please enter your name' }
         </div>
     )
 
 }
 
-/* Fake setState of our creation */
-function setState(newState){
-    Object.assign(state, newState);
-    Render()
-}
-/* Rendering, as usual, but inside it's own function */
-function Render(){
-    ReactDOM.render(<App/>, rootElement);
-}
-Render()
-
+ReactDOM.render(<Greetings/>, rootElement);
