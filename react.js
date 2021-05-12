ddb9981b21724fe2ea0ae91286ecd4914a89c23c
LetMeDream <raulalfonzo66@gmail.com>
2021-05-12 04:55:38 -0400

    using 'useEffect' dependency array as an optimization

--- react.js
+++ react.js
@@ -7,10 +7,13 @@ function Greetings(){
     React.useEffect( () => {
         window.localStorage.setItem('name', name);
         name ? document.title = `Welcome, ${name.charAt(0).toUpperCase() + name.slice(1) }` : document.title = "REACT'S BASICS" ;
-    });
+        /* Just to show the empty array ([]) optimization */
+        console.log('rendered');
+
+    }, [name]);
 
     return(
-        <div className='container mt-2'>
+        <div>
             <label htmlFor="name">Name</label>
             <br/>
             <input value={name} onChange={ (event) => setName(event.target.value) } type="text"/>
@@ -21,4 +24,21 @@ function Greetings(){
 
 }
 
-ReactDOM.render(<Greetings/>, rootElement);
+function App(){
+
+    const [counter, setCounter] = React.useState(0);
+
+    function handleClick(){
+        setCounter( counter + 1 )
+    }
+
+    return(
+        <div className='container mt-2'>
+            <button onClick={ handleClick } className="btn btn-primary">{counter}</button>
+            <Greetings></Greetings>
+        </div>
+    )
+
+}
+
+ReactDOM.render(<App/>, rootElement);
