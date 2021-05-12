b21cf6e7affe30e208c5e79b52ef169b718ca51b
LetMeDream <raulalfonzo66@gmail.com>
2021-05-12 06:30:17 -0400

    Creating a custom hook for storing a STRING state into our localStorage

--- react.js
+++ react.js
@@ -1,15 +1,23 @@
 let rootElement = document.getElementById('root');
 
-function Greetings(){
+function useLocalStorageState(key, defaultValue = ''){
 
     /* Let's "optimize it" using a lazy loader */
-    const [name, setName] = React.useState(() => window.localStorage.getItem('name'));
+    const [state, setState] = React.useState(() => window.localStorage.getItem(key) || defaultValue );
 
     React.useEffect( () => {
-        window.localStorage.setItem('name', name);
-        name ? document.title = `Welcome, ${name.charAt(0).toUpperCase() + name.slice(1) }` : document.title = "REACT'S BASICS" ;
+        window.localStorage.setItem(key, state);
+        state ? document.title = `Welcome, ${state.charAt(0).toUpperCase() + state.slice(1) }` : document.title = "REACT'S BASICS" ;
         /* Just to show the empty array ([]) optimization */
-    }, [name]);
+    }, [state,key]);
+
+    return [state, setState]
+
+}
+
+function Greetings(){
+
+    const [name, setName] = useLocalStorageState('name', '');
 
     return(
         <div>
